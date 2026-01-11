f3e4be72c486a6f7eb28875c09013ef80d8745eb
plakna <plakna@plakna.at>
2026-01-11 18:31:14 +0100

    added: new activity property

--- custom_components/lg_hombot/vacuum.py
+++ custom_components/lg_hombot/vacuum.py
@@ -1,19 +1,29 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-
-from homeassistant.components.vacuum import STATE_CLEANING, STATE_DOCKED, STATE_PAUSED, STATE_RETURNING, StateVacuumEntity
-
-from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from __future__ import annotations
 
 from typing import Any
 from urllib.parse import quote
 
-from .const import DOMAIN, SPEED_NORMAL, SPEED_TURBO, SUPPORTED_SERVICES, SUPPORTED_SPEEDS
+from homeassistant.components.vacuum import StateVacuumEntity, VacuumActivity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import (
+    DOMAIN,
+    SPEED_NORMAL,
+    SPEED_TURBO,
+    SUPPORTED_SERVICES,
+    SUPPORTED_SPEEDS,
+)
 
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entites: AddEntitiesCallback) -> None:
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
     """Setup of Entry."""
     # get url of hombot
     url = config_entry.data.get("url")
@@ -21,7 +31,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, asyn
 
     # create instance
     hombot_vac = HombotVacuum(name, url)
-    async_add_entites([hombot_vac])
+    async_add_entities([hombot_vac])
 
 
 class HombotVacuum(StateVacuumEntity):
@@ -35,33 +45,33 @@ class HombotVacuum(StateVacuumEntity):
 
         self._url = url
 
-        self._state = None
+        self._activity = VacuumActivity.IDLE
         self._battery_level = None
         self._fan_speed = SPEED_NORMAL
         self._fan_speed_list = SUPPORTED_SPEEDS
 
+        self._attr_unique_id = "hombot_" + self._url
+
     @property
     def unique_id(self) -> str | None:
-        return "hombot_" + self._url
+        return self._attr_unique_id
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
         device_info = DeviceInfo(
-            identifiers={
-                (DOMAIN, self.unique_id)
-            },
+            identifiers={(DOMAIN, self.unique_id)},
             name=self._name,
             manufacturer="LG",
             model="Hombot",
-            configuration_url=self._url
+            configuration_url=self._url,
         )
         return device_info
 
     @property
-    def state(self) -> str:
-        """Return the status of the vacuum."""
-        return self._state
+    def activity(self) -> VacuumActivity:
+        """Return the activity of the vacuum."""
+        return self._activity
 
     @property
     def battery_level(self) -> int:
@@ -76,7 +86,7 @@ class HombotVacuum(StateVacuumEntity):
     def fan_speed_list(self) -> list[str]:
         return self._fan_speed_list
 
-    async def async_update(self):
+    async def async_update(self) -> None:
         """Update status properties."""
         # generate url
         url = self._url + "status.txt"
@@ -94,58 +104,67 @@ class HombotVacuum(StateVacuumEntity):
             attributes[name] = var.strip('"')
 
         # assign properties
-        self._state = self.convert_state(attributes["JSON_ROBOT_STATE"])
-        self._battery_level = int(attributes["JSON_BATTPERC"])
-        if attributes["JSON_TURBO"] == "true":
+        self._activity = self.convert_state(attributes.get("JSON_ROBOT_STATE"))
+        self._battery_level = int(attributes.get("JSON_BATTPERC", "0"))
+
+        if attributes.get("JSON_TURBO") == "true":
             self._fan_speed = SPEED_TURBO
         else:
             self._fan_speed = SPEED_NORMAL
 
-    def convert_state(self, current_state) -> str:
-        """Converts the status of hombot to that of HomeAssistant -> see https://developers.home-assistant.io/docs/core/entity/vacuum/#states"""
+    def convert_state(self, current_state: str | None) -> VacuumActivity:
+        """Convert the Hombot status to Home Assistant VacuumActivity."""
         match current_state:
             case "CHARGING":
-                return STATE_DOCKED
+                return VacuumActivity.DOCKED
             case "BACKMOVING_INIT":
-                return STATE_CLEANING
+                return VacuumActivity.CLEANING
             case "WORKING":
-                return STATE_CLEANING
+                return VacuumActivity.CLEANING
             case "PAUSE":
-                return STATE_PAUSED
+                return VacuumActivity.PAUSED
             case "HOMING":
-                return STATE_RETURNING
-            case"DOCKING":
-                return STATE_RETURNING
+                return VacuumActivity.RETURNING
+            case "DOCKING":
+                return VacuumActivity.RETURNING
+            case _:
+                return VacuumActivity.IDLE
 
     async def async_start(self, **kwargs: Any) -> None:
         """Turn the vacuum on."""
-        self._state = STATE_CLEANING
+        self._activity = VacuumActivity.CLEANING
+        self.async_write_ha_state()
         await self.query('{"COMMAND":"CLEAN_START"}')
 
     async def async_pause(self, **kwargs: Any) -> None:
         """Turn the vacuum on."""
-        self._state = STATE_PAUSED
+        self._activity = VacuumActivity.PAUSED
+        self.async_write_ha_state()
         await self.query('{"COMMAND":"PAUSE"}')
 
     async def async_return_to_base(self, **kwargs: Any) -> None:
         """Turn the vacuum off."""
-        self._state = STATE_RETURNING
+        self._activity = VacuumActivity.RETURNING
+        self.async_write_ha_state()
         await self.query('{"COMMAND":"HOMING"}')
 
     async def async_stop(self, **kwargs: Any) -> None:
         """Turn the vacuum off."""
-        self._state = STATE_RETURNING
+        self._activity = VacuumActivity.RETURNING
+        self.async_write_ha_state()
         await self.query('{"COMMAND":"HOMING"}')
 
     async def async_set_fan_speed(self, fan_speed: str, **kwargs: Any) -> None:
         """Sets fan speed."""
         self._fan_speed = fan_speed
+        self.async_write_ha_state()
+
         if fan_speed == SPEED_TURBO:
             await self.query('{"COMMAND":{"TURBO":"true"}}')
         else:
             await self.query('{"COMMAND":{"TURBO":"false"}}')
 
-    async def query(self, command):
+    async def query(self, command: str) -> bool:
         """Execute command."""
         # generate url
         url = self._url + "json.cgi?" + quote(command)
